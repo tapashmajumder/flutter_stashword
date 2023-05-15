d2fe53ab98531103cb4b64d68b36727120d6f2a8
Tapash Majumder <tapash.majumder@stashword.com>
2023-05-15 19:54:16 +0700

    Try using secure storage

--- lib/database.dart
+++ lib/database.dart
@@ -1,19 +1,26 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import 'item.dart';
 
+interface class C1 {};
+
 class Database {
   static const String _itemBoxName = 'items';
+  static const String _encryptionKeyName = 'encryptionKey';
 
   static Future<void> init({String? subdir}) async {
     await Hive.initFlutter(subdir);
     Hive.registerAdapter(ItemAdapter());
   }
 
-  static Future<void> open() async {
-    await Hive.openBox<Item>(_itemBoxName);
+  static Future<void> open({FlutterSecureStorage secureStorage = const FlutterSecureStorage()}) async {
+    final String encryptionKeyString = await secureStorage.read(key: _encryptionKeyName) ?? await _generateAndStoreEncryptionKey(secureStorage);
+    final encryptionKey = base64Url.decode(encryptionKeyString);
+    await Hive.openBox<Item>(_itemBoxName, encryptionCipher: HiveAesCipher(encryptionKey));
   }
 
   static Box<Item> getItemBox() {
@@ -22,6 +29,12 @@ class Database {
 
   static Future<void> close() async {
     await Hive.close();
+  }
+
+  static Future<String> _generateAndStoreEncryptionKey(FlutterSecureStorage secureStorage) async {
+    final key = base64Url.encode(Hive.generateSecureKey());
+    await secureStorage.write(key: _encryptionKeyName, value: key);
+    return key;
   }
 }
 
