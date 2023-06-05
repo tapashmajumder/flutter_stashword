b18d57f8c982a4f407f881c5e1344e6ab10660fb
Tapash Majumder <tapash.majumder@stashword.com>
2023-06-05 14:47:50 +0700

    Better splitview

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:Stashword/ui/split_view/app_menu.dart';
 import 'package:Stashword/ui/split_view/example/first_page.dart';
 import 'package:Stashword/ui/split_view/split_view.dart';
 import 'package:flutter/material.dart';
@@ -16,7 +17,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.indigo,
       ),
-      home: SplitView(),
+      home: const SplitView(
+        menu: AppMenu(),
+        content: FirstPage(),
+      ),
     );
   }
 }
--- lib/ui/split_view/split_view.dart
+++ lib/ui/split_view/split_view.dart
@@ -3,38 +3,43 @@ import 'package:Stashword/ui/split_view/example/first_page.dart';
 import 'package:flutter/material.dart';
 
 class SplitView extends StatelessWidget {
+  const SplitView({
+    Key? key,
+    // menu and content are now configurable
+    required this.menu,
+    required this.content,
+    // these values are now configurable with sensible default values
+    this.breakpoint = 600,
+    this.menuWidth = 240,
+  }) : super(key: key);
+  final Widget menu;
+  final Widget content;
+  final double breakpoint;
+  final double menuWidth;
+
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
-    const breakpoint = 600.0;
     if (screenWidth >= breakpoint) {
       // widescreen: menu on the left, content on the right
       return Row(
         children: [
-          // use SizedBox to constrain the AppMenu to a fixed width
           SizedBox(
-            width: 240,
-            // TODO: make this configurable
-            child: AppMenu(),
+            width: menuWidth,
+            child: menu,
           ),
-          // vertical black line as separator
           Container(width: 0.5, color: Colors.black),
-          // use Expanded to take up the remaining horizontal space
-          Expanded(
-            // TODO: make this configurable
-            child: FirstPage(),
-          ),
+          Expanded(child: content),
         ],
       );
     } else {
       // narrow screen: show content, menu inside drawer
       return Scaffold(
-        body: FirstPage(),
-        // use SizedBox to contrain the AppMenu to a fixed width
+        body: content,
         drawer: SizedBox(
-          width: 240,
+          width: menuWidth,
           child: Drawer(
-            child: AppMenu(),
+            child: menu,
           ),
         ),
       );
