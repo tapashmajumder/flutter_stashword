d25be1c9750a09f8b1668ed272ad4a45cab5a1ab
Tapash Majumder <tapash.majumder@stashword.com>
2023-06-21 20:21:22 +0700

    Added ViewTopRow

--- lib/ui/item/view_password.dart
+++ lib/ui/item/view_password.dart
@@ -1,5 +1,39 @@
 import 'package:flutter/material.dart';
 
+class ViewTopWidget extends StatelessWidget {
+  const ViewTopWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(children: [
+        const CircleAvatar(
+          backgroundColor: Colors.lightBlue,
+          child: Text("AB"),
+        ),
+        const SizedBox(width: 10),
+        Column(crossAxisAlignment: CrossAxisAlignment.start, mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text("Instagram", style: Theme.of(context).textTheme.titleMedium),
+          const SizedBox(height: 10),
+          Text("shared by user2@example.com", style: Theme.of(context).textTheme.titleSmall),
+        ]),
+        Expanded(child: Container()),
+        Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text("Share", style: Theme.of(context).textTheme.titleSmall),
+        ]),
+        Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          IconButton(
+            onPressed: () {
+            },
+            icon: const Icon(Icons.chevron_right),
+          )
+        ])
+      ]),
+    );
+  }
+}
+
 class ViewRowWidget extends StatelessWidget {
   const ViewRowWidget({Key? key}) : super(key: key);
 
@@ -17,7 +51,7 @@ class ViewRowWidget extends StatelessWidget {
           IconButton(
             onPressed: () {
             },
-            icon: Icon(Icons.open_in_new),
+            icon: const Icon(Icons.open_in_new),
           )
         ])
       ]),
