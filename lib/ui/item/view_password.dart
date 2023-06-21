bfcd3470db36d020f9ab3fee1835f1be4950c77c
Tapash Majumder <tapash.majumder@stashword.com>
2023-06-21 22:09:19 +0700

    First version of ViewPassword widget.

--- lib/ui/item/view_password.dart
+++ lib/ui/item/view_password.dart
@@ -1,4 +1,25 @@
+import 'package:Stashword/model/item_models.dart';
 import 'package:flutter/material.dart';
+
+class ViewPasswordWidget extends StatelessWidget {
+  final PasswordModel model;
+  const ViewPasswordWidget({Key? key, required this.model}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        ViewTopWidget(),
+        const SizedBox(height: 20),
+        Container(height: 1, color: Colors.grey),
+        ViewRowWidget(),
+        Container(height: 1, color: Colors.grey),
+      ]),
+    );
+  }
+}
+
 
 class ViewTopWidget extends StatelessWidget {
   const ViewTopWidget({Key? key}) : super(key: key);
